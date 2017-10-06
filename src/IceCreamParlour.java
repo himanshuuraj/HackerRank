14a7063552f7d58796e5114e9c96e4589b082fb7
himanshu <himanshu@himanshus-MacBook-Air.local>
2017-10-06 10:29:37 +0530

    commiting IceCreamParlour

---
+++ src/IceCreamParlour.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class IceCreamParlour {
+
+    public static void main(String[] args) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int m = in.nextInt();
+            int n = in.nextInt();
+            for(int a_i=0; a_i < n; a_i++){
+                int num = in.nextInt();
+                map.computeIfPresent(num, (k, v) -> map.put(-1 * k,a_i+1));
+                map.putIfAbsent(num,a_i+1);
+            }
+            for (Map.Entry<Integer, Integer> entry : map.entrySet())
+            {
+                int key = entry.getKey();
+                int secKey = m - key;
+                //int value = entry.getValue();
+                if(key == secKey) secKey = -1 * key;
+                if(map.containsKey(key) && map.containsKey(secKey))
+                {
+                    System.out.println(map.get(key) + " " + map.get(secKey));
+                    break;
+                }
+            }
+            map.clear();
+        }
+    }
+}
