eaf678a119c59aab2595f71bfb75b63aa25b44cc
sumit <sumit@vizexperts.com>
2017-10-09 19:16:27 +0530

    commiting the half made BFS

--- src/BFS.java
+++ src/BFS.java
@@ -1,79 +1,58 @@
 import java.io.*;
 import java.util.*;
 
-// This class represents a directed graph using adjacency list
-// representation
 class BFS
 {
-    private int V;   // No. of vertices
-    private LinkedList<Integer> adj[]; //Adjacency Lists
+    static int weight = 0;
+    //static List<Node> stack = new ArrayList();
+    static int[] output;
+    /*public static class Node{
+        int data;
+        int weight;
+        boolean visited;
+        public Node(int data,int weight, boolean visited){
+            this.data = data;
+            this.weight = weight;
+            this.visited = visited;
+        }
+    }*/
 
-    // Constructor
-    BFS(int v)
-    {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i=0; i<v; ++i)
-            adj[i] = new LinkedList();
-    }
+    public static class Graph {
+        int size;  // This is basically the number of nodes
+        Map<Integer,List<Integer>> edges = new HashMap();
+        Stack<Integer> stack = new Stack();
+        public Graph(int size) {
+            this.size = size;
+        }
 
-    // Function to add an edge into the graph
-    void addEdge(int v,int w)
-    {
-        adj[v].add(w);
-    }
+        public void addEdge(int first, int second) {
+            List al = new ArrayList();
+            if(edges.containsKey(first)){
+                al = edges.get(first);
+                al.add(second);
+            }else{
+                al.add(second);
+                edges.put(first,al);
+            }
+        }
 
-    // prints BFS traversal from a given source s
-    void BFS(int s)
-    {
-        // Mark all the vertices as not visited(By default
-        // set as false)
-        boolean visited[] = new boolean[V];
-
-        // Create a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-
-        // Mark the current node as visited and enqueue it
-        visited[s]=true;
-        queue.add(s);
-
-        while (queue.size() != 0)
-        {
-            // Dequeue a vertex from queue and print it
-            s = queue.poll();
-            System.out.print(s+" ");
-
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-            Iterator<Integer> i = adj[s].listIterator();
-            while (i.hasNext())
-            {
-                int n = i.next();
-                if (!visited[n])
-                {
-                    visited[n] = true;
-                    queue.add(n);
+        public void shortestReach(int startId) { // 0 indexed
+            while(!stack.isEmpty()) {
+                int startid = stack.pop();
+                if(output[startid - 1] < 0) continue;
+                output[startId - 1] = weight * 6;
+                List<Integer> al = edges.get(startId);
+                weight++;
+                for (Integer i : al) {
+                    output[startId - 1] = weight * 6;
+                    if(!)
+                    stack.add(i);
                 }
             }
         }
     }
-
-    // Driver method to
-    public static void main(String args[])
-    {
-        BFS g = new BFS(4);
-
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
-
-        System.out.println("Following is Breadth First Traversal "+
-                "(starting from vertex 2)");
-
-        g.BFS(2);
+    public static void main(String[] args) {
+        int size = 6;
+        output = new int[size];
     }
 }
